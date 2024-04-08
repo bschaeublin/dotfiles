90f722b3ede5800ddab93a7f30e6f7f1ec77e629
Schäublin Benjamin <benjamin.schaeublin@cmiag.ch>
2024-04-08 09:39:21 +0200

    change buffers with filename in statusline

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -1,3 +1,19 @@
+--- @param trunc_width number trunctates component when screen width is less then trunc_width
+--- @param trunc_len number truncates component to trunc_len number of chars
+--- @param hide_width number hides component when window width is smaller then hide_width
+--- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+--- return function that can format the component accordingly
+local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+    return function(str)
+        local win_width = vim.fn.winwidth(0)
+        if hide_width and win_width < hide_width then return ''
+        elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+            return str:sub(1, trunc_len) .. (no_ellipsis and '' or '...')
+        end
+        return str
+    end
+end
+
 return {
     "nvim-lualine/lualine.nvim",
     lazy = false,
@@ -9,17 +25,7 @@ return {
         },
         sections = {
             lualine_a = { "mode" },
-            lualine_b = { 
-                {
-                    "filename",
-                    symbols = {
-                        modified = "󰧞",
-                        readonly = "",
-                        new = "",
-                        unnamed = "",
-                    },
-                }
-            },
+            lualine_b = { "buffers" },
             lualine_c = {},
             lualine_x = {},
             lualine_y = {},
@@ -41,8 +47,22 @@ return {
             lualine_z = {},
         },
         tabline = {      
-            lualine_b = {"branch"},
-            lualine_c = { "buffers" 
+            lualine_b = {
+                {
+                    "branch",
+                    fmt = trunc(150,50,nil),
+                }
+            },
+            lualine_c = { 
+                {
+                    "filename",
+                    symbols = {
+                        modified = "󰧞",
+                        readonly = "",
+                        new = "",
+                        unnamed = "",
+                    },
+                }
             },
             lualine_x = {},
             lualine_y = { "fileformat", "filetype" },
