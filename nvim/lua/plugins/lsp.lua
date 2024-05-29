3da3d5728051ea54c17bc2b0adbf6a2c6638d659
Benjamin Schaeublin <11610660+bschaeublin@users.noreply.github.com>
2024-05-29 16:09:36 +0200

    fix lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -16,6 +16,9 @@ return {
             local cmp = require('cmp')
 
             cmp.setup({
+                sources = {
+                    {name = 'nvim_lsp'},
+                },
                 mapping = cmp.mapping.preset.insert({
                     ['<CR>'] = cmp.mapping.confirm({select = false}),
                     ['<C-Space>'] = cmp.mapping.complete(),
