c83ec33af705e0beb0f9f11ef5077693ceca8162
Benjamin Schaeublin <11610660+bschaeublin@users.noreply.github.com>
2024-06-06 08:54:17 +0200

    enable copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -2,9 +2,9 @@ return {
     {
         'github/copilot.vim',
         event = 'InsertEnter',
-        enabled = function()
-            -- only run in private setup
-            return vim.fn.hostname() == 'DESKTOP-F76LQQS'
-        end,
+        -- enabled = function()
+        --     -- -- only run in private setup
+        --     -- return vim.fn.hostname() == 'DESKTOP-F76LQQS'
+        -- end,
     }
 }
