346b2aa89fc5862e7ced156abe83c885e46f6958
Benjamin Schaeublin <11610660+bschaeublin@users.noreply.github.com>
2024-08-16 00:33:36 +0200

    add flash and use zbirrenbaum copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,7 +1,20 @@
 return {
     {
-        'github/copilot.vim',
-        event = 'InsertEnter',
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                suggestion = {
+                    auto_trigger = true,
+                    keymap = {
+                        accept = "<M-l>",
+                        next = "<M-j>",
+                        prev = "<M-k>",
+                    }
+                }
+            })
+        end,
         -- enabled = function()
         --     -- -- only run in private setup
         --     -- return vim.fn.hostname() == 'DESKTOP-F76LQQS'
